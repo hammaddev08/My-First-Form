aa23408476be064d311cc1d6c3a572ff67e19990
MuhammadRafay7 <infinite272005@gmail.com>
2026-07-20 09:28:13 +0500

    Add ID card flip feature and basic layout

---
+++ rafay/lecture-5/script.js
@@ -0,0 +1,16 @@
+function sub(num1,num2) {
+    return num1 - num2;
+}
+
+const 
+result = sub(10, 5);
+console.log(result);
+
+const 
+sum = add(7, 5);
+console.log(sum);
+
+function add(num1,num2) {
+    return num1 + num2;
+}
+
